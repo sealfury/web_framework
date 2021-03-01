e30d32e8b2962e4a33c5aea940660b4c8972592b
sealfury <willspecker@gmail.com>
2021-03-01 12:23:37 +0100

    creates user class with props interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello')
+import { User } from './models/User'
+
+const user = new User({ })
+
+user.set({ name: 'name' })
+user.set({ age: 99 })
+
+console.log(user.get('name'))
+console.log(user.get('age'))
