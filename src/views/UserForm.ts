e21402dc31ca8e6bbcb0bb5a9b558147a4e4938c
sealfury <willspecker@gmail.com>
2021-03-02 14:42:12 +0100

    outlines method for change username click event

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -14,7 +14,14 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
     }
+  }
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input')
+    const name = input.value
+    this.model.set({ name })
   }
 
   onSetAgeClick = (): void => {
@@ -28,7 +35,7 @@ export class UserForm {
         <div>User Name: ${this.model.get('name')}</div>
         <div>User Age: ${this.model.get('age')}</div>
         <input />
-        <button>Click Me</button>
+        <button class="set-name">Change Name</button>
         <button class="set-age">Set Random Age</button>
       </div>
     `
