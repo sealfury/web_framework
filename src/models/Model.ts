9ecff7cd44600addc3ebb6d54d1c7aef823b8321
sealfury <willspecker@gmail.com>
2021-03-02 13:13:32 +0100

    completes extraction to class Model refactors class User renames class sync to ApiSync for clarity

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -1,9 +1,9 @@
-import { AxiosPromise } from "axios";
+import { AxiosPromise, AxiosResponse } from 'axios'
 
 type Callback = () => void
 
 interface ModelAttributes<T> {
-  set(value: T): void;
+  set(value: T): void
   getAll(): T
   get<K extends keyof T>(key: K): T[K]
 }
@@ -13,10 +13,59 @@ interface Sync<T> {
   save(data: T): AxiosPromise
 }
 
+interface HasId {
+  id?: number
+}
 
 interface Events {
   on(eventName: string, callback: Callback): void
   trigger(eventName: string): void
 }
 
-export class Model {}
+export class Model<T extends HasId> {
+  constructor(
+    private attributes: ModelAttributes<T>,
+    private events: Events,
+    private sync: Sync<T>
+  ) {}
+
+  get on() {
+    return this.events.on
+  }
+
+  get trigger() {
+    return this.events.trigger
+  }
+
+  get get() {
+    return this.attributes.get
+  }
+
+  set(update: T): void {
+    this.attributes.set(update)
+    this.events.trigger('change')
+  }
+
+  fetch(): void {
+    const id = this.get('id')
+
+    if (typeof id !== 'number') {
+      throw new Error('Cannot retrieve data without an id!')
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data)
+    })
+  }
+
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger('save')
+      })
+      .catch(() => {
+        this.trigger('error')
+      })
+  }
+}
