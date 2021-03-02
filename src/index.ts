03b4115b4cd6183924c810128f9a6972252a59d2
sealfury <willspecker@gmail.com>
2021-03-02 14:00:15 +0100

    adds div to index.html to append form to on render

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import { User } from './models/User'
+import { UserForm } from './views/UserForm'
 
-const collection = User.buildUserCollection()
+const userForm = new UserForm(document.getElementById('root'))
+
+userForm.render()
