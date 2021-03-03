137afd668bd816b25fbc2c7a283664ff3bbabd78
sealfury <willspecker@gmail.com>
2021-03-03 10:39:53 +0100

    displays collection view in html

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
-import { UserEdit } from './views/UserEdit'
-import { User } from './models/User'
+import { UserList } from './views/UserList'
+import { Collection } from './models/Collection'
+import { User, UserProps } from './models/User'
+import { UserForm } from './views/UserForm'
 
-const user = User.buildUser({ name: 'Sti', age: 7 })
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json)
+  }
+)
 
-const root = document.getElementById('root')
+users.on('change', () => {
+  const root = document.getElementById('root')
 
-if (root) {
-  const userEdit = new UserEdit(root, user)
+  if (root) {
+    new UserList(root, users).render()
+  }
+})
 
-  userEdit.render()
-
-  console.log(userEdit)
-} else {
-  throw new Error('Root element not found')
-}
+users.fetch()
